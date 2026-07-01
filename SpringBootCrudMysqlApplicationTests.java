cc234375bffdc20e5288f44be0d8b65aa102bf3d
MD MUSARRAF PARWEZ <musarrafparwez7@gmail.com>
2026-07-01 11:54:39 +0530

    Add files via upload

---
+++ SpringBootCrudMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbm.spring_boot_crud_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
